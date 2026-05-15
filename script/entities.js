5b806e811034860e295f8ec4811ab8e9952db94d
Xander Thomson <xthomson@smail.pembinatrails.ca>
2026-05-15 14:05:37 -0500

    I don't know

--- script/entities.js
+++ script/entities.js
@@ -5,17 +5,18 @@
   in "game.js".
 */
 
-class Cannon {
-  x = 0;
-  element = document.createElement('img');
-  shot = null;
-  canShoot = true;
-  constructor() {
-    this.element.src = "images/Ship.png";
-    this.element.style.scale = '2';
-    setInterval(this.update(this.element), 1);
+class Cannon { // Defining what a Cannon (Player) is...
+  x = 0; // Horizontal position
+  element = document.createElement('img'); // The HTML element representing this cannon
+  shot = null; // The shot from the cannon (only one at a time)
+  canShoot = true; // Can the cannon shoot?
+  constructor() { // When the cannon object is created...
+    this.element.src = "images/Ship.png"; // Set the element's image source
+    this.element.style.width = '4vh'; // Set its width appropriately
+    document.getElementById('game').appendChild(this.element);
+    setInterval(() => this.update(), 1); // Update its position every millisecond
   }
-  update() {
-    this.element.style.transform = 'translate(' + this.x + ', 40vh)';
+  update() { // Every time we are told to update the position...
+    this.element.style.transform = 'translate(' + (this.x / 10) + 'vh, 80vh)'; // Update the position by translating the image
   }
 }
--- script/game.js
+++ script/game.js
@@ -7,20 +7,35 @@
 let game;
 
 // Entity initializers
-let cannon = new Cannon();
+let cannon;
 
 // Miscallenious initializers
+let keys = [];
 
+// Game functions
+function updateCannon() {
+  if (keys.includes('ArrowLeft') || keys.includes('a') || keys.includes('A')) {cannon.x -= 1.5;}
+  if (keys.includes('ArrowRight') || keys.includes('d') || keys.includes('D')) {cannon.x += 1.5;}
+  // Fix caps lock issues
+  
+  if (cannon.x < 0) {cannon.x = 0;}
+  if (cannon.x > 575) {cannon.x = 575;}
+}
 
+// Document-related things
 document.addEventListener('DOMContentLoaded', () => { // When the page loads...
   game = document.getElementById('game'); // Define "game" with the "game" div
-  game.appendChild(cannon.element);
+  cannon = new Cannon(); // Create the cannon (player)
+  setInterval(updateCannon, 1);
 });
 
-document.addEventListener('keydown', () => {
-
+document.addEventListener('keydown', (e) => { // When a key is pressed...
+  // e = KeyboardEvent
+  // e.key = String representation of KeyboardEvent
+  if (!keys.includes(e.key)) {keys.push(e.key);} // Add the key to the array of pressed keys if it hasn't been already
+  console.log(e.key);
 });
 
-document.addEventListener('keyup', () => {
-  
+document.addEventListener('keyup', (e) => { // When a key is released...
+  if (keys.includes(e.key)) {keys.splice(keys.indexOf(e.key), 1);} // Remove the key from the array of pressed keys if it hasn't been already
 });
